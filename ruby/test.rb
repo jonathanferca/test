bf4c8e363e612b2add6abd6a2e495ad948d6bf03
Jonathan Fernandez <yoyofcbmx@gmail.com>
2014-03-24 15:45:30 -0600

    method modified

--- ruby/test.rb
+++ ruby/test.rb
@@ -3,7 +3,7 @@ class StringChecker
     print "Enter the word you want to check: "
     word = gets.chomp
     
-    puts word.downcase.gsub(/\s+/, "") == word.downcase.gsub(/\s+/, "").reverse ? true : false
+    puts word.downcase.gsub(/\s+/, "") == word.downcase.gsub(/\s+/, "").reverse
   end
 end
 
